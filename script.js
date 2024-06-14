4692cbd26fb7c9983e2f163376d5b13d4460d514
Maahir Jain <maahirdjain@gmail.com>
2024-06-15 01:45:57 +0530

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     switch(Math.floor(Math.random() * 3)) {
         case 0:
@@ -11,8 +14,38 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Choose rock, paper, or scissors!");
-    return humanChoice.toLowerCase();
+
+    while (humanChoice.toLowerCase() !== "rock" 
+        && humanChoice.toLowerCase() !== "paper" 
+        && humanChoice.toLowerCase() !== "scissors") {
+        humanChoice = prompt("You made an unavailable selection. Choose rock, paper, or scissors!");
+    }
+
+    return humanChoice.toLowerCase();    
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function capitalize(str) {
+    return str.at(0).toUpperCase() + str.slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie -- ${capitalize(humanChoice)} VS. ${computerChoice}.`);
+        humanScore += 0.5;
+        computerScore += 0.5;
+    } else if (humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock"
+    ) {
+        console.log(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}.`);
+        computerScore++;
+    } else {
+        console.log(`You win! ${capitalize(humanChoice)} beats ${computerChoice}.`);
+        humanScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
