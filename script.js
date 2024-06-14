0d854062843da12e46fa4e2e98cd0d78d3e9253d
Maahir Jain <maahirdjain@gmail.com>
2024-06-15 01:12:44 +0530

    Write the logic to get the human choice

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose rock, paper, or scissors!");
+    return humanChoice.toLowerCase();
+}
